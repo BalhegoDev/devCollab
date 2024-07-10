b94c5ade810599f59a784e1ad521adb8162269c1
Bros12Lek <adgamesbalhego@hotmail.com>
2024-07-10 10:36:54 -0300

    Adoro dar a bund@

--- src/components/checkboxInput.jsx
+++ src/components/checkboxInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function CheckboxInput({id,label}){
+export default function CheckboxInput({id,label,value,setValue}){
     return(
         <label style={{display:"flex",flexDirection:"row",alignItems:"center", position:"relative"}} id={id}>
             <input
